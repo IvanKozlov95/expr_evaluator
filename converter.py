0438a62ac74ee1a05dcc8e704ac4b16e1f33a5ed
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-04-21 23:18:02 -0700

    Added check for stupid spaces and negative numbers

--- converter.py
+++ converter.py
@@ -3,11 +3,17 @@
 import os
 import sys
 
-if len(sys.argv) != 2:
-	print('Usage: ./converter.py [infix expression]')
-	sys.exit()
+DEBUG = 0
+test = "(-2 + -2)"
 
-infix = sys.argv[1]
+if DEBUG is 0:
+	if len(sys.argv) != 2:
+		print('Usage: ./converter.py [infix expression]')
+		sys.exit()
+
+	infix = sys.argv[1]
+else:
+	infix = test
 
 # operators
 operators = ['+', '-', '*', '/', '%', '^']
@@ -29,13 +35,19 @@ def opcmp(op1, op2):
 def puts(msg):
 	print(msg, end='', flush=True)
 
+lastnumber = False
+sign = 1
 for token in infix:
 	if token == ' ':
+		continue
+	if token == '-' or token == '+' and lastnumber is False:
+		sign = -1 if token == '-' else 1
 		continue
 	if token in operators:
 		while len(stack) > 0 and opcmp(stack[-1], token) >= 0:
 			rpn.append(stack.pop())
 		stack.append(token)
+		lastnumber = False
 	elif token == '(':
 		stack.append(token)
 	elif token == ')':
@@ -43,7 +55,10 @@ for token in infix:
 			rpn.append(stack.pop())
 		stack.pop()
 	else:
-		rpn.append(token)
+		rpn.append(str(sign * int(token)))
+		sign = 1
+		lastnumber = True
+
 while len(stack) != 0:
 	rpn.append(stack.pop())
 
