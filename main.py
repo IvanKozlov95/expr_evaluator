1fdd046204c77a72a26ab517659f9c3544488242
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-04-22 03:19:18 -0700

    Added main

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from evaluator import eval_rpn
 from util import *
 
 DEBUG = 0
-test =  "2 --( 2 + * -32 )"
+test =  "+"
 
 _input = get_input() if DEBUG is 0 else test
 rpn = infix_to_postfix(tokenize(_input))
-print(eval_rpn(rpn))
+print(rpn)
+if len(rpn) is 0:
+	print("nothing in rpn")
+else:
+	print(eval_rpn(rpn))
