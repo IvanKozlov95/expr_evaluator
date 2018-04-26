c41c5a4de75b15e22dc249f63fcf9e6b87ce8129
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-04-26 00:31:42 -0700

    Removed checks for incorrect input

--- evaluator/converter.py
+++ evaluator/converter.py
@@ -15,53 +15,20 @@ def opcmp(op1, op2):
 	return get_precedence(op1) - get_precedence(op2)
 
 def tokenize(input):
-	result = []
 	tokens = input.split(' ')
 	for token in tokens:
-		i = 0
-		while i < len(token):
-			c = token[i]
-			if c in numbers:
-				tail = token[i:]
-				for j in range(0, len(tail)):
-					if tail[j] not in numbers:
-						break
-				if j is len(tail) - 1 and tail[j] in numbers:
-					j += 1
-				result.append(token[i:i+j])
-				i += j
-			elif c in operators or c == '(' or c == ')':
-				result.append(c)
-				i += 1
-			else:
+		for c in token:
+			if c not in numbers and c not in operators and c not in "()":
 				raise Exception("Invalid symbol '{}'".format(c))
-	return result
-
-def incorrect_token(token, pos):
-	raise Exception("Incorrect token '{}' at pos {}".format(token, pos))	
+	return tokens
 
 def infix_to_postfix(infix):
 	sign = 1
 	rpn = []
 	stack = []
-	lastop = True
 
 	for idx, token in enumerate(infix):
-		if (token is '-' or token is '+') and lastop is True:
-			sign = -1 if token is '-' else 1
-			if idx < len(infix) - 1:
-				if infix[idx + 1] in operators:
-					incorrect_token(infix[idx + 1], idx + 1)
-				elif infix[idx + 1] is '(':
-					rpn.append('-1')
-					stack.append('*')
-					sign = 1
-			else:
-				raise Exception("Not enough tokens")
-			continue
 		if token in operators:
-			if lastop is True:
-				incorrect_token(token, idx)
 			while len(stack) > 0 and opcmp(stack[-1], token) >= 0:
 				rpn.append(stack.pop())
 			stack.append(token)
