41c1d94657ca99e4f850d0548fb38b8cbf7854c1
Ivan Kozlov <ikozlov9000@gmail.com>
2018-05-01 19:22:19 -0700

    Added pdf resources:

---
+++ stack.py
@@ -0,0 +1,15 @@
+class Stack:
+    def __init__(self):
+        pass
+    
+    def push(self):
+        pass
+    
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def isEmpty(self):
+        pass
